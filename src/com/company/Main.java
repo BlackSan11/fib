6408fd6954b9f41b43a5e58ca0ee471743bc4a11
Bogdan <bhelpdig@gmail.com>
2017-11-17 19:13:11 +0300

    коммит

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,18 +3,29 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(getNFib(47));
+        System.out.println(getNFib(48));
     }
     /**
      * @param nFib - максимальное число до переполнения 47
      */
     private static int getNFib(int nFib) {
-        int[] fibs = new int[nFib+1];
+        int[] fibs = new int[2];
         fibs[0] = 0;
         fibs[1] = 1;
-        for (int i = 2; i < nFib; i++) {
-            fibs[i] = fibs[i - 1] + fibs[i - 2];
+        int fibRes = 0;
+        int buf;
+        if(nFib == 1){
+            fibRes = 0;
+        } else if(nFib == 2){
+            fibRes = 1;
+        } else{
+            for (int i = 2; i < nFib; i++){
+                buf = fibs[1];
+                fibs[1] = fibs[0] + fibs[1];
+                fibs[0] = buf;
+            }
+            fibRes = fibs[1];
         }
-        return fibs[nFib-1];
+        return fibRes;
     }
 }
