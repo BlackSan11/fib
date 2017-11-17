4fb63019c2842d01642c4556fea01ab1dbea7cd5
Bogdan <bhelpdig@gmail.com>
2017-11-17 18:43:19 +0300

    коммит

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(getNFib(48));
+    }
+    /**
+     * @param nFib - максимальное число до переполнения 47
+     */
+    private static double getNFib(int nFib) {
+        int[] fibs = new int[nFib+1];
+        fibs[0] = 0;
+        fibs[1] = 1;
+        for (int i = 2; i < nFib; i++) {
+            fibs[i] = fibs[i - 1] + fibs[i - 2];
+        }
+        return fibs[nFib-1];
+    }
+}
